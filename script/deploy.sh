cafae77a80b9f3f14a6bb6b630ec351847dd06ef
Jens Krause <jens@rasta.online>
2020-06-23 13:47:43 +0700

    deploy script update

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+git status
+bundle install
+JEKYLL_ENV="production" bundle exec jekyll build
+git status
 git add .
 git commit -m"[skip travis] Automated build"
 git remote set-url origin https://$ROBOT@github.com/Solomonic/theming.rasta_online.git
