b6a16a75005483acee9a4502b4e0c97248d748d6
Jens Krause <jens@rasta.online>
2020-06-24 16:21:57 +0700

    ci build fixed with webp support

--- script/install_IM.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-wget https://www.imagemagick.org/download/ImageMagick-7.0.10-21.tar.gz
-tar -xzvf ImageMagick-7.0.10-21.tar.gz
-cd ImageMagick-7.0.10-21 && ./configure --prefix=/usr && make && sudo make install
