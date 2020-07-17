2cb6f79523a7ccc48f723eb5dc77695e4f293d77
Jens Krause <jens@rasta.online>
2020-07-17 18:57:17 +0700

    new amp carousel in music

--- _plugins/amp_filter.rb
+++ _plugins/amp_filter.rb
@@ -85,7 +85,60 @@ module Jekyll
       # Return the html as plaintext string
       doc.to_s
     end
+
+    def amp_images_fill(input, fill = true, wi = nil, he = nil)
+      doc = Nokogiri::HTML.fragment(input);
+      doc.css('img:not([width])').each do |image|
+        if wi && he
+          image['width']  = wi
+          image['height'] = he
+        else
+          if image['src'].start_with?('http://', 'https://')
+            src = image['src']
+          else
+            src = File.join(Dir.pwd, 'docs', image['src'])
+          end
+          begin
+            size = FastImage.size(src)
+            image['width']  = size[0]
+            image['height'] = size[1]
+          rescue Exception => e
+            puts 'Unable to get image dimensions for "' + src + '".[Error: ' + e.to_s + ']'
+          end
+        end
+      end
+      doc.css('img').each do |image|
+        image.name = "amp-img"
+
+        image["style"] ="max-width:" + image['width'] + "px"
+
+        image['layout'] = "fill" if fill
+      end
+
+      doc.css('picture').each do |picture|
+        amp_img = picture.css('amp-img')
+        picture.add_next_sibling(amp_img) unless amp_img.empty?
+        picture.remove
+      end
+
+      doc.css('amp-img').each do |amp_img|
+        noscript = Nokogiri::XML::Node.new "noscript", doc
+
+        noscript_img = amp_img.dup
+        noscript_img.remove_attribute('layout')
+        noscript_img.name = 'img'
+
+        noscript.add_child(noscript_img)
+
+        amp_img.add_child(noscript)
+      end
+
+      doc.to_s
+    end
+
   end
 end
+
+
 
 Liquid::Template.register_filter(Jekyll::AmpFilter)
